ed5c4275d926a01e4d3597db84157b6f3f3bd83a
TornadoGhost <tornadoghost97@gmail.com>
2023-11-08 17:27:15 +0200

    added RegisterController

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+
+class RegisterController extends Controller
+{
+    public function store(StoreUserRequest $request) {
+        
+    }
+}
